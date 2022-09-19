06bcaccf89ab52a9a60a530d9e6908f9d939e6db
calipf1975 <calipf1975@gmail.com>
2022-09-20 00:53:44 +0300

    Дз_1 + исправления по стилю

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ const goods = [
   ];
   
   const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p><button class="" type="button">Купить</button></div>`;
+    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p><button class="btn">Buy</button></div>`;
   };
   
   const renderGoodsList = (list) => {
-    document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item.title, item.price)).join('');
+  document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item.title, item.price)).join('');
   }
 //   Метод map-возвращает новый массив, 
 //   а метод join служит для объединения элементов массива в строку через указанный разделитель.
