a42465906238a77f6edbb49e3255d2e6edd497ec
calipf1975 <calipf1975@gmail.com>
2022-09-22 10:39:56 +0300

    ДЗ_2+исправления по стилю

--- main.js
+++ main.js
@@ -1,19 +1,85 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ];
+// const goods = [
+//     { title: 'Shirt', price: 150 },
+//     { title: 'Socks', price: 50 },
+//     { title: 'Jacket', price: 350 },
+//     { title: 'Shoes', price: 250 },
+//   ];
   
-  const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p><button class="btn">Buy</button></div>`;
-  };
+//   const renderGoodsItem = (title, price) => {
+//     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p><button class="btn">Buy</button></div>`;
+//   };
   
-  const renderGoodsList = (list) => {
-  document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item.title, item.price)).join('');
+//   const renderGoodsList = (list) => {
+//   document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item.title, item.price)).join('');
+//   }
+// //   Метод map-возвращает новый массив, 
+// //   а метод join служит для объединения элементов массива в строку через указанный разделитель.
+// //   Сокращаем код-за счет промежуточной переменной.
+  
+//   renderGoodsList(goods);
+class GoodsItem {
+  constructor(title, price){
+    this.title = title;
+    this.price = price;
   }
-//   Метод map-возвращает новый массив, 
-//   а метод join служит для объединения элементов массива в строку через указанный разделитель.
-//   Сокращаем код-за счет промежуточной переменной.
   
-  renderGoodsList(goods);
+  render() {
+    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p><button class="btn">Buy</button></div>`;
+  }
+  
+}
+
+class GoodsList {
+  constructor(){
+    this.goods = [];
+  }
+  
+  fetchGoods(){
+    this.goods = [
+      { title: 'Shirt', price: 150 },
+      { title: 'Socks', price: 50 },
+      { title: 'Jacket', price: 350 },
+      { title: 'Shoes', price: 250 },
+    ];
+  }
+  
+  render(){
+    let listHtml="";
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    })
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+
+class BasketItem{
+  constructor(){
+  }
+  render(){
+    return `<div></div>`;
+  }
+}
+class BasketList{
+  constructor(){
+    this.goods = [];
+  }
+  fetchGoods(){
+    this.goods = [];
+  }
+  render(){
+    let listHtml="";
+    this.goods.forEach(good => {
+      const basketItem = new BasketItem(good.title, good.price);
+      listHtml += basketItem.render();
+    })
+    document.querySelector('.').innerHTML = listHtml;
+  }
+}
+const basketlist = new BasketList();
+basketlist.fetchGoods();
+basketlist.render();
