51baf50c0dbc77bdafc0009b8544fd0dbc16f490
calipf1975 <calipf1975@gmail.com>
2022-09-29 21:55:25 +0300

    ДЗ-3 без *

--- main.js
+++ main.js
@@ -1,31 +1,55 @@
 // const goods = [
-//     { title: 'Shirt', price: 150 },
-//     { title: 'Socks', price: 50 },
-//     { title: 'Jacket', price: 350 },
-//     { title: 'Shoes', price: 250 },
+//     { product_name: 'Shirt', price: 150 },
+//     { product_name: 'Socks', price: 50 },
+//     { product_name: 'Jacket', price: 350 },
+//     { product_name: 'Shoes', price: 250 },
 //   ];
   
-//   const renderGoodsItem = (title, price) => {
-//     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p><button class="btn">Buy</button></div>`;
+//   const renderGoodsItem = (product_name, price) => {
+//     return `<div class="goods-item"><h3>${product_name}</h3><p>${price}</p><button class="btn">Buy</button></div>`;
 //   };
   
 //   const renderGoodsList = (list) => {
-//   document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item.title, item.price)).join('');
+//   document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item.product_name, item.price)).join('');
 //   }
 // //   Метод map-возвращает новый массив, 
 // //   а метод join служит для объединения элементов массива в строку через указанный разделитель.
 // //   Сокращаем код-за счет промежуточной переменной.
   
 //   renderGoodsList(goods);
+const API_URL ='https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+function makeGETRequest(url, callback){
+    return new Promise((resolve, reject) =>{
+      var xhr;
+        if (window.XMLHttpRequest) {
+    // Chrome, Mozilla, Opera, Safari
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    // Internet Explorer
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText);
+    }
+  }
+
+  xhr.open('GET', url, true);
+  xhr.send();
+  });
+  
+
+}
 class GoodsItem {
-  constructor(title, price, img){
-    this.title = title;
+  constructor(product_name, price){
+    this.product_name = product_name;
     this.price = price;
-    this.img = img;
   }
   
   render() {
-    return `<div class="goods-item"><img class="img" src="${this.img}"><h3>${this.title}</h3><p>${this.price}</p><button class="btn">Buy</button></div>`;
+    return `<div class="goods-item"><img class="img" src=""><h3>${this.product_name}</h3><p>${this.price}</p><button class="btn">Buy</button></div>`;
   }
   
 }
@@ -36,18 +60,26 @@ class GoodsList {
   }
   
   fetchGoods(){
-    this.goods = [
-      { title: 'Shirt', price: 150, img:"img/Shirt.webp"},
-      { title: 'Socks', price: 50, img:"img/Socks.webp" },
-      { title: 'Jacket', price: 350, img:"img/Jacket.webp" },
-      { title: 'Shoes', price: 250, img:"img/Shoes.webp" },
-    ];
+    // this.goods = [
+    //   { product_name: 'Shirt', price: 150, img:"img/Shirt.webp"},
+    //   { product_name: 'Socks', price: 50, img:"img/Socks.webp" },
+    //   { product_name: 'Jacket', price: 350, img:"img/Jacket.webp" },
+    //   { product_name: 'Shoes', price: 250, img:"img/Shoes.webp" },
+    // ];
+    makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+      this.goods = JSON.parse(goods);
+      this.render();
+      let allSum=list.priceGoodsSum();
+      console.log(allSum);
+      
+    })
   }
+
   
   render(){
     let listHtml="";
     this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.title, good.price, good.img);
+      const goodItem = new GoodsItem(good.product_name, good.price);
       listHtml += goodItem.render();
     })
     document.querySelector('.goods-list').innerHTML = listHtml;
@@ -64,10 +96,15 @@ class GoodsList {
 }
 
 const list = new GoodsList();
+// list.fetchGoods(()=>{
+//   list.render();
+//   let allSum=list.priceGoodsSum();
+//   console.log(allSum);
+// });
 list.fetchGoods();
-list.render();
-let allSum=list.priceGoodsSum();
-console.log(allSum);
+
+
+
 
 class BasketItem{
   constructor(){
@@ -80,13 +117,24 @@ class BasketList{
   constructor(){
     this.goods = [];
   }
-  fetchGoods(){
-    this.goods = [];
-  }
+
+  // Добавление товара в корзину
+  addToBasket() {
+ 
+   
+}
+ // Удаление товара из корзины 
+ deleteFromBasket() {
+
+}
+ // Считаем стоимость товаров в корзине
+ calcAllGoods() {
+
+}
   render(){
     let listHtml="";
     this.goods.forEach(good => {
-      const basketItem = new BasketItem(good.title, good.price);
+      const basketItem = new BasketItem(good.product_name, good.price);
       listHtml += basketItem.render();
     })
     document.querySelector('.').innerHTML = listHtml;
